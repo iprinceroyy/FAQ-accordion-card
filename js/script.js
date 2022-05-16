91fad7e6635a14927288f3f119ae8a0f6df9183d
Prince Roy <princeku.ray143@gmail.com>
2022-05-16 20:42:10 +0530

    update index.scss

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 "use strict";
 const question = document.querySelectorAll(".question");
-const arrow = document.querySelectorAll(".arrow");
 
-//console.log(question.length);
 for (let i = 1; i <= question.length; i++) {
-    const ans = document.querySelector(`.question-${i}`);
-    document
-        .querySelector(`.arrow-${i}`)
-        .addEventListener("click", function() {
-            document.querySelector(`.answer-${i}`).classList.toggle("show");
-            //this.classList.toggle(".animation");
-            //console.log(this.classList);
-            document.querySelector(`.arrow-${i}`).classList.toggle("animation");
-        });
+    const arrow = document.querySelector(`.arrow-${i}`);
+    arrow.addEventListener("click", () => {
+        document.querySelector(`.arrow-${i}`).classList.toggle("animation");
+        document.querySelector(`.answer-${i}`).classList.toggle("show");
+        document.querySelector(`.question-${i}`).classList.toggle("typo-theme");
+        console.log(document.querySelector(`.question-${i}`));
+    });
 }
